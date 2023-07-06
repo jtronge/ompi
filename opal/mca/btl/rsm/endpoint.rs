36276b118f06e684d7958a34bc391cda35546924
Jake Tronge <jtronge3@gmail.com>
2023-07-06 13:59:39 -0600

    Update endpoint.rs safety comment

--- opal/mca/btl/rsm/endpoint.rs
+++ opal/mca/btl/rsm/endpoint.rs
@@ -11,7 +11,6 @@ use crate::fifo::FIFO;
 /// Info about a given endpoint
 #[repr(C)]
 pub(crate) struct Endpoint {
-    // pub map: Arc<Mutex<SharedRegionMap>>,
     pub _base: opal_list_item_t,
     pub rank: Rank,
     pub fifo: FIFO,
@@ -22,18 +21,15 @@ impl Endpoint {
     pub fn new(map: Arc<Mutex<SharedRegionMap>>, rank: Rank) -> Result<Endpoint> {
         let fifo = FIFO::new(Arc::clone(&map), rank);
         let _base = unsafe {
-            // SAFETY: This will be initialized by obj_construct_rs()
-            MaybeUninit::uninit().assume_init()
+            let mut base = MaybeUninit::uninit();
+            obj_construct_rs(base.as_mut_ptr());
+            // SAFETY: This was initialized by obj_construct_rs()
+            base.assume_init()
         };
-        let mut ep = Endpoint {
+        Ok(Endpoint {
             _base,
-            // map,
             rank,
             fifo,
-        };
-        unsafe {
-            obj_construct_rs(&mut ep._base);
-        }
-        Ok(ep)
+        })
     }
 }
