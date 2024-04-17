51270294bc0fcd2a5f00669503d060292471dcc4
Jake Tronge <jtronge@lanl.gov>
2024-04-17 11:55:26 -0600

    Add ELEMENT_COUNT for MPI_Get_elements_x

--- ompi/mpi/bindings/ompi_bindings/c_type.py
+++ ompi/mpi/bindings/ompi_bindings/c_type.py
@@ -126,6 +126,14 @@ class TypeCountArray(Type):
         return f'const {count_type} {self.name}[]'
 
 
+@Type.add_type('ELEMENT_COUNT')
+class ElementCountType(Type):
+    """Special count type for MPI_Get_element_x"""
+
+    def type_text(self, enable_count=False):
+        return 'MPI_Count *'
+
+
 @Type.add_type('PARTITIONED_COUNT')
 class TypePartitionedCount(Type):
     """Count type for partitioned communication functions."""
